5034cd24ad1a01270de5f2be6c77e32121b1cbe9
Frank van der Stad <frank.vanderstad@flinders.nl>
2026-08-20 13:10:03 +0200

    feat: add favo-icon

--- resources/views/layouts/lucy.blade.php
+++ resources/views/layouts/lucy.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Lucy-Rhea')</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('images/favicon-lucy.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/favicon-lucy.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Arizonia&family=Montserrat:wght@400;500;600&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
